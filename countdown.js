2e43999e6eb0f55f8c1e5be7819736a89a1da611
jiegeng <99167212+completel@users.noreply.github.com>
2022-03-14 22:22:28 +0800

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,8 +1,10 @@
+// ele指元素 timer指的就是倒计时的时间
 function countdown(ele, timer) {
   let time = setInterval( () => {
     ele.innerText = timer;
     timer--;
     if (timer === 0) {
+      // 时间为0，则停止定时器，以免减成负数。
       clearInterval(time);
       ele.innerText = '结束啦';
     } else {
